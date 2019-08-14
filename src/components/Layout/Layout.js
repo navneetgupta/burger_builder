76fad0bde75a84be7927d5cda67220138f5cbacb
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-14 19:29:37 +0400

    Lecture 152: Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
+import classes from "./Layout.module.css";
 
 const Layout = props => (
   <Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Fragment>
 );
 
