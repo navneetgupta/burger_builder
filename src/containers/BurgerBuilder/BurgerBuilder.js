e156290a2e2d9fbc2f9e1a18efd4664ee1ffd550
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-14 19:29:45 +0400

    Lecture 152: Starting Implementation of The Burger Builder Container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Burger</p>
+        <p>Builder Control</p>
+      </Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
