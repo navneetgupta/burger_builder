fb3f7c08565439058e76c3ad7e0e05668cc6e0a8
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-16 17:09:49 +0400

    Lecture 176: Reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Sidedrawer from "../Navigation/Sidedrawer/Sidedrawer";
 
-const Layout = props => (
-  <Fragment>
-    <Toolbar />
-    <Sidedrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = {
+    sideBarClosed: false
+  };
+  toggleSidebarHandler = () => {
+    const oldState = this.state.sideBarClosed;
+    this.setState({ sideBarClosed: !oldState });
+  };
+  render() {
+    return (
+      <Fragment>
+        <Toolbar />
+        <Sidedrawer
+          show={!this.state.sideBarClosed}
+          clicked={this.toggleSidebarHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Sidedrawer.module.css";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Logo from "../../Logo/Logo";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const Sidedrawer = props => {
+  let attachedClasses = [classes.Sidedrawer, classes.Close];
+  if (props.show) {
+    attachedClasses = [classes.Sidedrawer, classes.Open];
+  }
   return (
-    <div className={classes.Sidedrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Fragment>
+      <Backdrop show={props.show} clicked={props.clicked} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Fragment>
   );
 };
 
