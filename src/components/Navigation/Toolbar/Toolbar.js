80e8b2f9ceafa408493e755d88131bfbb045a446
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-16 16:49:50 +0400

    Lecture 175: More about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = props => {
   return (
-    <header className={classes.Toolbar}>
+    <header className={[classes.Toolbar, classes.DesktopOnly].join(" ")}>
       <div>Menu</div>
       <div className={classes.Logo}>
         <Logo />
