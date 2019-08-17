75667b5956e5c5bf87c79160f6c46454c169fc89
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-17 10:09:30 +0400

    Lecture 185: Basic version complete

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,28 @@
+import React, { Fragment, Component } from "react";
+import classes from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../../components/Navigation/Sidedrawer/Sidedrawer";
+
+class Layout extends Component {
+  state = {
+    sideBarActive: false
+  };
+  toggleSidebarHandler = () => {
+    const oldState = this.state.sideBarActive;
+    this.setState({ sideBarActive: !oldState });
+  };
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.toggleSidebarHandler} />
+        <Sidedrawer
+          show={this.state.sideBarActive}
+          clicked={this.toggleSidebarHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
