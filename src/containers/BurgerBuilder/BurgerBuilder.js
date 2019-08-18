477695a71cd1df1e7f1d1254468d53a89c973e7a
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-18 19:16:14 +0400

    Lecture 210: Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler";
 
 const INGREDIENS_PRICE = {
   salad: 0.5,
@@ -139,4 +140,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
