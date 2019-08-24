2c8f8b41a0971bfe371a250f98a26479a796a0fb
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-24 09:43:39 +0400

    Lecture 255. Order Submission & Passing Data Between Pages

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,10 +10,10 @@ const CheckoutSummary = props => {
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" onClick={props.checkoutCancelled}>
+      <Button btnType="Danger" clicked={props.checkoutCancelled}>
         Cancel
       </Button>
-      <Button btnType="Success" onClick={props.checkoutContinued}>
+      <Button btnType="Success" clicked={props.checkoutContinued}>
         Continue
       </Button>
     </div>
