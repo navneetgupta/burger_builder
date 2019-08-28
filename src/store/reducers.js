1df196aa265a8b0dd72af4c793e24fee11a09360
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-28 17:29:30 +0400

    Lecture 302. Installing Redux and React Redux

---
+++ src/store/reducers.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "./action";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducers = (state = initialState, action) => {
+  return state;
+};
+
+export default reducers;
