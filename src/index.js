4fc7ddddada12c9af2f509dbd73dee1f48deac5a
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-28 17:45:48 +0400

    Lecture 303. Basic Redux Setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import reducers from "./store/reducers";
+
+const store = redux.createStore(reducers);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
