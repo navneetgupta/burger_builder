6816c4143a2205100884a6331fe319f6817570dd
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-01 21:48:32 +0400

    Lecture 354. Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -24,8 +25,26 @@ export const auth = (email, password) => {
   console.log(email, password);
   return dispatch => {
     dispatch(authStart());
-    setTimeout(() => {
-      dispatch(authSuccess({ success: true }));
-    }, 2000);
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAhby0-pgUeJEYA3oOy8ytVjqba__kGlvM",
+        authData
+      )
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFailed(err));
+      });
+    // setTimeout(() => {
+    //   dispatch(authSuccess({ success: true }));
+    // }, 2000);
   };
 };
