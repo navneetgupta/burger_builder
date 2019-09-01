13c47f3202135d53b79cf1fc6647eb791894200b
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-01 22:13:36 +0400

    Lecture 356. Storing the Token

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,22 +1,34 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-  loading: false
+  loading: false,
+  token: null,
+  userId: null,
+  error: null
 };
 
 const reducers = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
       return {
-        loading: true
+        loading: true,
+        token: null,
+        userId: null,
+        error: null
       };
     case actionTypes.AUTH_SUCCESS:
       return {
-        loading: false
+        loading: false,
+        userId: action.authData.email,
+        token: action.authData.idToken,
+        error: null
       };
     case actionTypes.AUTH_FAILED:
       return {
-        loading: false
+        loading: false,
+        token: null,
+        userId: null,
+        error: action.error
       };
     default:
       return state;
