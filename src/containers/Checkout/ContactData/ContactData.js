9bf158136c380eadbbc92c34428419de26f2515f
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-04 17:55:36 +0400

    Lecture 374. Sharing the Validation Method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
 import withErrorHandler from "../../../hoc/withErrorHandler";
 import * as actionCreators from "../../../store/actions";
-import { updateObject } from "../../../shared/utility";
+import { updateObject, checkValidity } from "../../../shared/utility";
 
 class ContactData extends Component {
   state = {
@@ -110,31 +110,10 @@ class ContactData extends Component {
     this.props.onOrderBurger(order, this.props.token);
   };
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-    if (!rules) return true;
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.trim().length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.trim().length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-      // var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
-      // isValid = re.test(value);
-    }
-    return isValid;
-  };
   inputVlaueChangeHandler = (event, inputIdentifier) => {
     const updatedFromElt = updateObject(this.state.orderForm[inputIdentifier], {
       value: event.target.value,
-      valid: this.checkValidity(
+      valid: checkValidity(
         event.target.value,
         this.state.orderForm[inputIdentifier].validation
       ),
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,24 @@ export const updateObject = (oldObject, updatedValues) => {
     ...updatedValues
   };
 };
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+  if (!rules) return true;
+  if (rules.required) {
+    isValid = value.trim() !== "" && isValid;
+  }
+  if (rules.minLength) {
+    isValid = value.trim().length >= rules.minLength && isValid;
+  }
+  if (rules.maxLength) {
+    isValid = value.trim().length <= rules.maxLength && isValid;
+  }
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value) && isValid;
+    // var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
+    // isValid = re.test(value);
+  }
+  return isValid;
+};
